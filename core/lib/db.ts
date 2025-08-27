a2a373d73d8608cdb1c7fc1243c585b59d2bb4ae
Akis05 <ahmed.med6355@gmail.com>
2025-08-27 13:58:27 +0300

    update v3.0.4

--- core/lib/db.ts
+++ core/lib/db.ts
@@ -1,9 +1,17 @@
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-    var prisma: PrismaClient | undefined;
-  }
+  // Empêche la recréation du client en dev/hot reload
+  // ⚠️ Nécessaire dans Next.js App Router
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
 
-export const db = globalThis.prisma || new PrismaClient();
+export const db =
+  globalThis.prisma ||
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
