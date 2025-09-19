b601ba7b1d80d4f15b34b73bc5be97c3bb37d0bf
Akis05 <ahmed.med6355@gmail.com>
2025-09-19 23:15:12 +0300

    delete getCurrentUser

--- app/(user)/accompagnateur/page.tsx
+++ app/(user)/accompagnateur/page.tsx
@@ -1,5 +1,4 @@
 import { Dashboard } from "@/core/view/accompagnateur/accompagnement-body";
-import { getCurrentUser } from "@/core/hooks/use-current-user";
 import { GetUserCookies } from "@/core/hooks/use-get-user-cookies";
  
 const UserDashboard = async () => {
