606edfaf76b2774acf4c8f22f65b5ad348f5b50a
Akis05 <ahmed.med6355@gmail.com>
2025-09-21 22:35:04 +0300

    update mv6

--- core/view/planning/planning-view.tsx
+++ core/view/planning/planning-view.tsx
@@ -494,7 +494,7 @@ export default function PlanningCalendar({
                   open(
                     <CustomModal>
                       <PlanningForm
-                        details={planning}
+                        details={{ ...planning!, visit: [] }}
                         accompanimentId={accompanimentId}
                         userId={userId}
                       />
@@ -744,17 +744,6 @@ export default function PlanningCalendar({
                           size="sm"
                           variant="outline"
                           className="bg-white/70 hover:bg-white"
-                          onClick={() => {
-                            open(
-                              <CustomModal>
-                                <PlanningForm
-                                  details={{ ...planning, visit: [visit] }}
-                                  accompanimentId={accompanimentId}
-                                  userId={userId}
-                                />
-                              </CustomModal>
-                            );
-                          }}
                         >
                           <Edit className="w-4 h-4" />
                         </Button>
