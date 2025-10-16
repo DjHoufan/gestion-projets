0f177f09b3aca1f636026555c500189cf8ea107e
Akis05 <ahmed.med6355@gmail.com>
2025-10-16 10:15:34 +0300

    update v0.0.0.3

--- core/servers/superviseur/route.ts
+++ core/servers/superviseur/route.ts
@@ -1,68 +1,75 @@
+import { db } from "@/core/lib/db";
 import { Hono } from "hono";
- 
+
 const app = new Hono()
   .onError((err, c) => {
     return c.json({ error: err }, 400);
   })
-    .get("/:accId", async (c) => {
+  .get("/:accId", async (c) => {
     const { accId } = c.req.param();
-    const data = await db.accompaniment.findFirst({
+
+    const reponse = await db.users.findFirst({
       where: {
         id: accId,
       },
       include: {
-        file: true,
-        media: true,
-        map: {
+        accompaniments: {
           include: {
-            accompaniment: {
+            file: true,
+            media: true,
+            map: {
               include: {
+                accompaniment: {
+                  include: {
+                    users: true,
+                    members: true,
+                  },
+                },
+              },
+            },
+            planning: {
+              include: {
+                visit: true,
                 users: true,
-                members: true,
               },
             },
-          },
-        },
-        planning: {
-          include: {
-            visit: true,
-            users: true,
-          },
-        },
-        project: true,
-        members: {
-          include: {
-            leave: true,
-            emargements: true,
-          },
-        },
-        purchases: {
-          include: {
-            purchaseItems: true,
-          },
-        },
-        rencontre: {
-          include: {
-            files: true,
-            signatures: {
+            project: true,
+            members: {
               include: {
-                member: true,
+                leave: true,
+                emargements: true,
               },
             },
-            visit: true,
+            purchases: {
+              include: {
+                purchaseItems: true,
+              },
+            },
+            rencontre: {
+              include: {
+                files: true,
+                signatures: {
+                  include: {
+                    member: true,
+                  },
+                },
+                visit: true,
+                users: true,
+              },
+            },
+            conflits: {
+              include: {
+                files: true,
+                partieImpliques: true,
+                users: true,
+              },
+            },
             users: true,
           },
         },
-        conflits: {
-          include: {
-            files: true,
-            partieImpliques: true,
-            users: true,
-          },
-        },
-        users: true,
       },
     });
-    return c.json({ data });
-  })
+
+    return c.json({ reponse });
+  });
 export default app;
