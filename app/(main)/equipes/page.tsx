e590d58dd70340dc3748f86c10c709f35262c9c1
Akis05 <ahmed.med6355@gmail.com>
2025-11-05 16:36:57 +0300

    update v0.0.18

--- app/(main)/equipes/page.tsx
+++ app/(main)/equipes/page.tsx
@@ -1,30 +1,30 @@
 import { GetUserCookies } from "@/core/hooks/use-get-user-cookies";
-import { supabaseAdmin } from "@/core/supabase/client";
+// import { supabaseAdmin } from "@/core/supabase/client";
  
  
 import { EquipeBody } from "@/core/view/team/team-body";
 
 
-async function resetPasswords() {
-  const userIds = [
-  "13d2536e-bfab-4c07-b5eb-496715b53b27"
-  ];
+// async function resetPasswords() {
+//   const userIds = [
+//   "13d2536e-bfab-4c07-b5eb-496715b53b27"
+//   ];
 
-  for (const id of userIds) {
-    const { data, error } = await supabaseAdmin.auth.admin.updateUserById(id, {
-      password: "Password@55V1",
-    });
+//   for (const id of userIds) {
+//     const { data, error } = await supabaseAdmin.auth.admin.updateUserById(id, {
+//       password: "Password@55V1",
+//     });
 
-    console.log({data});
+//     console.log({data});
     
 
-    if (error) {
-      console.error(`Erreur pour ${id}:`, error.message);
-    } else {
-      console.log(`Mot de passe réinitialisé pour: ${id}`);
-    }
-  }
-}
+//     if (error) {
+//       console.error(`Erreur pour ${id}:`, error.message);
+//     } else {
+//       console.log(`Mot de passe réinitialisé pour: ${id}`);
+//     }
+//   }
+// }
 
 const Epuipes = async () => {
  
