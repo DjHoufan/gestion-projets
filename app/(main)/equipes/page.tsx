35c987b247effd25600258bb13e88dfdc1e882b5
Akis05 <ahmed.med6355@gmail.com>
2025-12-02 09:19:35 +0300

    update v0.0.24

--- app/(main)/equipes/page.tsx
+++ app/(main)/equipes/page.tsx
@@ -1,26 +1,9 @@
 import { GetUserCookies } from "@/core/hooks/use-get-user-cookies";
-import { supabaseAdmin } from "@/core/supabase/client";
+ 
 
 import { EquipeBody } from "@/core/view/team/team-body";
 
-async function resetPasswords() {
-  const userIds = ["190dc6ab-b1f3-4a04-8093-fdcb8893bb27"];
-
-  for (const id of userIds) {
-    const { data, error } = await supabaseAdmin.auth.admin.updateUserById(id, {
-      password: "Password@55V1",
-    });
-
-    console.log({ data });
-
-    if (error) {
-      console.error(`Erreur pour ${id}:`, error.message);
-    } else {
-      console.log(`Mot de passe réinitialisé pour: ${id}`);
-    }
-  }
-}
-
+ 
 const Epuipes = async () => {
   const permission = await GetUserCookies();
 
