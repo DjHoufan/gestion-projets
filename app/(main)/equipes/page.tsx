be598aa247c83be30dd2701e060057bdb3f4481c
Akis05 <ahmed.med6355@gmail.com>
2026-01-20 09:21:50 +0300

    rk-01

--- app/(main)/equipes/page.tsx
+++ app/(main)/equipes/page.tsx
@@ -1,14 +1,37 @@
 import { GetUserCookies } from "@/core/hooks/use-get-user-cookies";
+// import { supabaseAdmin } from "@/core/supabase/client";
  
 
 import { EquipeBody } from "@/core/view/team/team-body";
+
+
+//  export async function updateUserPassword(
+//   userId: string,
+//   newPassword: string
+// ) {
+//   const { data, error } = await supabaseAdmin.auth.admin.updateUserById(
+//     userId,
+//     {
+//       password: newPassword,
+//     }
+//   )
+
+//   if (error) {
+//     throw new Error(`Erreur mise à jour mot de passe : ${error.message}`)
+//   }
+
+//   return data
+// }
 
  
 const Epuipes = async () => {
   const permission = await GetUserCookies();
 
  
+//  const result = await updateUserPassword('df478b37-a21b-4123-8c79-398074e554d4', 'Password@55V1');
 
+//  console.log({result});
+ 
   return <EquipeBody permission={permission} />;
 };
 
